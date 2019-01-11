5282cf5401c688641f1ce74593023cac3409241a
Austin Chen <akrolsmir@gmail.com>
2019-01-10 20:08:01 -0800

    Add a melody textarea which rewrites with Gongche

---
+++ melodize.js
@@ -0,0 +1,24 @@
+const keyboardToGongche = {
+  's': '上',
+  'c': '尺',
+  'g': '工',
+  'f': '凡',
+  'l': '六',
+  'w': '五',
+  'y': '乙',
+  'h': '合',
+  'x': '四',
+  'i': '一',
+  '.': '。',
+  ',': '、',
+}
+
+function rewriteWithGongche() {
+  const melodyInput = document.getElementById('melody');
+  let output = "";
+  for (char of melodyInput.value) {
+    const gongche = keyboardToGongche[char];
+    output += gongche ? gongche : char;
+  }
+  melodyInput.value = output;
+}
