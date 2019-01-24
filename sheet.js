4604d8f2c1a069d1b2412c7b296c3e90b5303613
Austin Chen <akrolsmir@gmail.com>
2019-01-24 02:16:33 -0800

    Convert the test melody into quarter-based music

--- sheet.js
+++ sheet.js
@@ -14,38 +14,77 @@ const testLyrics =
 const testMelody =
 `四上 、一四上 。六尺 工尺 、工 。尺上_ 。上尺 工尺 、工六工 。尺 、四尺 。上一四_ 尺。上一 四 、四上 。一四合 、上尺 。工尺 、上 。六 工尺 、上 尺 。四上一四 、合 。上 上尺 、六 六。工尺上 、一四合 。尺 上 尺 、工 尺。工尺 、上 。合凡工 。工六 工尺 、上尺 工尺。工 、合四 。上尺 、上 。工 工上一四 、工 合 。四上一四 、合`
 
+const gongcheToJianpu = {
+  "一": "7.",
+  "四": "6.",
+  "合": "5.",
+  "上": "1",
+  "尺": "2",
+  "工": "3",
+  "凡": "4",
+  "六": "5",
+  "五": "6",
+  "乙": "7",
+}
 
+// TODO replace with function for key signature
+const jianpuToKey = {
+  "5.": "a/3",
+  "6.": "b/3",
+  "7.": "c/4",
+  "1": "d/4",
+  "2": "e/4",
+  "3": "f/4",
+  "4": "g/4",
+  "5": "a/4",
+  "6": "b/4",
+  "7": "c/5",
+}
+
+// Right now, output up a list of quarter notes
+// Format: [[gongche, lyric], ...]
+function buildQuarters(melody, lyrics) {
+  const unspacedLyrics = lyrics.replace(/\s/g, '');
+  let lyricIndex = 0;
+  let currentLyric = unspacedLyrics[lyricIndex];
+  const result = [];
+  for (char of melody) {
+    if (char == ' ') {
+      lyricIndex++;
+      currentLyric = unspacedLyrics[lyricIndex];
+    } else if (char in gongcheToJianpu) {
+      result.push([char, currentLyric]);
+      currentLyric = '-';
+    }
+  }
+  return result;
+}
 
 // Create an SVG renderer and attach it to the DIV element named "boo".
 var div = document.getElementById("boo")
 var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
 // Size our svg:
-renderer.resize(1000, 1000);
+renderer.resize(2500, 800);
 
 // And get a drawing context:
 var context = renderer.getContext();
 
 // Create a stave at position 10, 40 of width 400 on the canvas.
-var stave = new VF.Stave(10, 40, 500);
-var stave2 = new VF.Stave(410, 40, 400);
-var stave3 = new VF.Stave(10, 200, 400);
-var stave4 = new VF.Stave(410, 200, 400);
+var stave = new VF.Stave(10, 40, 2500);
+// var stave2 = new VF.Stave(410, 40, 400);
+// var stave3 = new VF.Stave(10, 200, 400);
+// var stave4 = new VF.Stave(410, 200, 400);
 
 
 // Add a clef and time signature.
-stave.addClef("treble").addTimeSignature("4/4");
+stave.addClef("treble").addTimeSignature("4/4").addKeySignature("D");
 
 // Connect it to the rendering context and draw!
 stave.setContext(context).draw();
 // stave2.setContext(context).draw();
-stave3.setContext(context).draw();
+// stave3.setContext(context).draw();
 // stave4.setContext(context).draw();
-
-function makeAnnotation(text) {
-  return new VF.Annotation(text)
-    .setVerticalJustification(VF.Annotation.VerticalJustify.BOTTOM);
-}
 
 function makeNote(key) {
   return new VF.StaveNote({clef: "treble", keys: [key], duration: "q"});
@@ -60,58 +99,47 @@ function makeText(text, line) {
     .setStave(stave);
 }
 
-var notes =[
-makeNote("c/5"),
-makeNote("d/5"),
-makeNote("f/4"),
-makeNote("c/5"),
-new VF.BarNote(),
-makeNote("e/5"),
-makeNote("f/4"),
-makeNote("d/5"),
-makeNote("f/4"),
-];
+function makeNotes(quarters) {
+  const melodyNotes = [];
+  const jianpuNotes = [];
+  const lyricsNotes = [];
+  for (let i = 0; i < quarters.length; i++) {
+    const [gongche, lyric] = quarters[i];
+    const jianpu = gongcheToJianpu[gongche];
+    const key = jianpuToKey[jianpu];
+    melodyNotes.push(makeNote(key));
+    jianpuNotes.push(makeText(jianpu));
+    lyricsNotes.push(makeText(lyric, 16));
+    if (i % 4 == 3) {
+      melodyNotes.push(new VF.BarNote());
+      jianpuNotes.push(new VF.BarNote());
+      lyricsNotes.push(new VF.BarNote());
+    }
+  }
+  return [melodyNotes, jianpuNotes, lyricsNotes];
+}
 
-var notes2 = [
-  makeText("c/55555"),
-  makeText("d/5"),
-  makeText("f/4"),
-  makeText("c/555"),
-  new VF.BarNote(),
-  makeText("e/5"),
-  makeText("f/4"),
-  makeText("d/5555555"),
-  makeText("f/4"),
-];
-
-var notes3 = [
-  makeText("堯", 16),
-  makeText("算", 16),
-  makeText("欣", 16),
-  makeText("逢", 16),
-  new VF.BarNote(),
-  makeText("照", 16),
-  makeText("照", 16),
-  makeText("照", 16),
-  makeText("照", 16),
-];
+var [melodyNotes, jianpuNotes, lyricsNotes] = 
+  makeNotes(buildQuarters(testMelody, testLyrics));
 
 // Create a voice in 4/4 and add above notes
-var voice = new VF.Voice({ num_beats: 8, beat_value: 4 });
-voice.addTickables(notes);
+const beats = buildQuarters(testMelody, testLyrics).length;
 
-var voice2 = new VF.Voice({ num_beats: 8, beat_value: 4 });
-voice2.addTickables(notes2);
+var melodyVoice = new VF.Voice({ num_beats: beats, beat_value: 4 });
+melodyVoice.addTickables(melodyNotes);
 
-var voice3 = new VF.Voice({ num_beats: 8, beat_value: 4 });
-voice3.addTickables(notes3);
+var jianpuVoice = new VF.Voice({ num_beats: beats, beat_value: 4 });
+jianpuVoice.addTickables(jianpuNotes);
+
+var lyricsVoice = new VF.Voice({ num_beats: beats, beat_value: 4 });
+lyricsVoice.addTickables(lyricsNotes);
 
 // Format and justify the notes to 400 pixels.
 var formatter = new VF.Formatter()
-  .joinVoices([voice, voice2, voice3])
-  .format([voice, voice2, voice3], 400);
+  .joinVoices([melodyVoice, jianpuVoice, lyricsVoice])
+  .format([melodyVoice, jianpuVoice, lyricsVoice], 2300);
 
 // Render voice
-voice.draw(context, stave);
-voice2.draw(context, stave);
-voice3.draw(context, stave);
+melodyVoice.draw(context, stave);
+jianpuVoice.draw(context, stave);
+lyricsVoice.draw(context, stave);
