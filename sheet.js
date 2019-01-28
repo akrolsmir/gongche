2084e40ba7c53076dc766adf2780822edde25c5f
Austin Chen <akrolsmir@gmail.com>
2019-01-28 02:09:27 -0800

    Create Note and LyricGroup objects

--- sheet.js
+++ sheet.js
@@ -1,5 +1,36 @@
 VF = Vex.Flow;
 
+class Note {
+  constructor(gongche) {
+    this.gongche = gongche;
+    this.duration; // TODO refactor to method
+    this.lyricGroup;
+  }
+  getLyric() {
+    const index = this.lyricGroup.children.indexOf(this);
+    return index == 0 ? this.lyricGroup.lyric : '-';
+  }
+  makeVexNotes() {
+    const jianpu = gongcheToJianpu[this.gongche];
+    const key = jianpuToKey[jianpu];
+
+    this.melodyNote = makeStaveNote(key, this.duration);
+    this.jianpuNote = makeTextNote(jianpu, 12, this.duration);
+    this.lyricsNote = makeTextNote(this.getLyric(), 16, this.duration);
+  }
+}
+
+class LyricGroup {
+  constructor(lyric) {
+    this.lyric = lyric;
+    this.children = [];
+  }
+  addNote(note) {
+    this.children.push(note);
+    note.lyricGroup = this;
+  }
+}
+
 const gongcheToJianpu = {
   "一": "7.",
   "四": "6.",
@@ -29,19 +60,20 @@ const jianpuToKey = {
 
 // Assign a lyric to each gongche symbol
 // Pass through other symbols unchanged
-// Format: [[gongche, lyric], ...]
+// Output: ['_', Note, Note, "."...]
 function assignLyrics(melody, lyrics) {
   const unspacedLyrics = lyrics.replace(/\s/g, '');
   let lyricIndex = 0;
-  let currentLyric = unspacedLyrics[lyricIndex];
+  let currentLyric = new LyricGroup(unspacedLyrics[lyricIndex]);
   const result = [];
   for (char of melody) {
     if (char == ' ') {
       lyricIndex++;
-      currentLyric = unspacedLyrics[lyricIndex];
+      currentLyric = new LyricGroup(unspacedLyrics[lyricIndex]);
     } else if (char in gongcheToJianpu) {
-      result.push([char, currentLyric]);
-      currentLyric = '-';
+      const note = new Note(char);
+      currentLyric.addNote(note);
+      result.push(note);
     } else {
       result.push(char);
     }
@@ -55,23 +87,23 @@ const BAR = '|';
 // Input: ['.', [g1, l1], ',', [...]...]
 // Format: ['|', [gongche, lyric, length (whole = 1, half = 2...)]]
 // Currently only works with . and ,
-function assignLengths(input) {
-  var output = [BAR]; // TODO use real marking object
+function assignLengths(notes) {
+  var output = []; // TODO use real marking object
   var symbols = ['。', '、'];
   const DOWNBEAT = '、';
   var symbolIndex = 1;
   var expectedSymbol = symbols[symbolIndex];
   var block = []
-  for (let i = 0; i < input.length; i++) {
-    var current = input[i];
-    if (current == expectedSymbol) {
+  for (const note of notes) {
+    if (note == expectedSymbol) {
       // This marks the end of a block.
       // Find durations for notes in the current block
-      const notesWithDurations = injectDurations(block);
-      for (const note of notesWithDurations) {
+      const injectedAndCropped = injectDurations(block);
+      for (const note of injectedAndCropped) {
+        note.makeVexNotes();
         output.push(note);
       }
-      if (current == DOWNBEAT) {
+      if (note == DOWNBEAT) {
         output.push(BAR);
       }
       block = [];
@@ -80,8 +112,8 @@ function assignLengths(input) {
       expectedSymbol = symbols[symbolIndex];
     } else {
       // Add any gongche notes to this current block
-      if (typeof current == "object") {
-        block.push(current);
+      if (typeof note == "object") {
+        block.push(note);
       }
     }
   }
@@ -90,36 +122,36 @@ function assignLengths(input) {
 
 // Input: [[gongche1, lyric1], ...]
 // Output: [[gongche, lyric, length (whole = 1, half = 2...)], ...]
-function injectDurations(quarters) {
-  // console.log(quarters);
-  if (quarters.length == 1) {
-    const [q1] = quarters;
-    q1.push('2') // HALF
-    return [q1];
+function injectDurations(notes) {
+  // console.log(notes);
+  if (notes.length == 1) {
+    const [n1] = notes;
+    n1.duration = '2' // HALF
+    return [n1];
   }
-  if (quarters.length == 2) {
-    const [q1, q2] = quarters;
-    q1.push('4'); // QUARTER
-    q2.push('4');
-    return [q1, q2];
+  if (notes.length == 2) {
+    const [n1, n2] = notes;
+    n1.duration = '4'; // QUARTER
+    n2.duration = '4';
+    return [n1, n2];
   }
-  if (quarters.length == 3) {
-    const [q1, q2, q3] = quarters;
-    q1.push('4'); // QUARTER
-    q2.push('8'); // EIGHTH
-    q3.push('8');
-    return [q1, q2, q3];
+  if (notes.length == 3) {
+    const [n1, n2, n3] = notes;
+    n1.duration = '4'; // QUARTER
+    n2.duration = '8'; // EIGHTH
+    n3.duration = '8';
+    return [n1, n2, n3];
   }
-  if (quarters.length >= 4) {
-    const [q1, q2, q3, q4] = quarters;
-    q1.push('8'); // EIGHTH
-    q2.push('8');
-    q3.push('8');
-    q4.push('8');
-    return [q1, q2, q3, q4];
+  if (notes.length >= 4) {
+    const [n1, n2, n3, n4] = notes;
+    n1.duration = '8'; // EIGHTH
+    n2.duration = '8';
+    n3.duration = '8';
+    n4.duration = '8';
+    return [n1, n2, n3, n4];
     // For now, drop the extra notes.
   }
-  throw `Invalid input of length ${quarters.length}`
+  throw `Invalid input of length ${notes.length}`
 }
 
 
@@ -131,11 +163,11 @@ function makeStave(index) {
   return stave;
 }
 
-function makeNote(key, duration) {
+function makeStaveNote(key, duration) {
   return new VF.StaveNote({clef: "treble", keys: [key], duration: duration});
 }
 
-function makeText(text, line, duration) {
+function makeTextNote(text, line, duration) {
   return new VF.TextNote({
     text: text,
     duration: duration
@@ -146,38 +178,27 @@ function makeText(text, line, duration) {
 }
 
 // Output: [ [melody1, jianpu1, lyrics1], ... ]
-function prepareNotesForVoices(notes) {
-  let melodyNotes = [];
-  let jianpuNotes = [];
-  let lyricsNotes = [];
-  let results = [[melodyNotes, jianpuNotes, lyricsNotes]];
+function splitStaves(notes) {
+  let stave = [];
+  let staves = [stave];
   let barCount = 0;
   for (note of notes) {
     if (note == BAR) {
       barCount++;
-      if (barCount == 5) {
-        // Create a new stave every 5 bars
+      if (barCount == 4) {
+        // Create a new stave every 4 bars
         barCount = 0;
-        melodyNotes = [];
-        jianpuNotes = [];
-        lyricsNotes = [];
-        results.push([melodyNotes, jianpuNotes, lyricsNotes]);
+        stave = [];
+        staves.push(stave);
       } else {
         // Mark a new measure
-        melodyNotes.push(new VF.BarNote());
-        jianpuNotes.push(new VF.BarNote());
-        lyricsNotes.push(new VF.BarNote());
+        stave.push(new VF.BarNote());
       }
     } else {
-      const [gongche, lyric, duration] = note;
-      const jianpu = gongcheToJianpu[gongche];
-      const key = jianpuToKey[jianpu];
-      melodyNotes.push(makeNote(key, duration));
-      jianpuNotes.push(makeText(jianpu, 12, duration));
-      lyricsNotes.push(makeText(lyric, 16, duration));
+      stave.push(note);
     }
   }
-  return results;
+  return staves;
 }
 
 // Return the number of quarter notes
@@ -198,19 +219,24 @@ function countQuarters(notes) {
 }
 
 // Output: [ [melodyVoice1, jianpuVoice1, lyricsVoice1], ...]
-function makeVoices(notes) {
+function makeVoices(staves) {
+  // TODO ugly reflection hack
+  function getTickables(stave, noteElement) {
+    return stave.map(note => note[noteElement] ? note[noteElement] : new VF.BarNote());
+  }
+
   const voices = [];
-  for (const [melodyNotes, jianpuNotes, lyricsNotes] of notes) {
-    const beats = countQuarters(melodyNotes);
+  for (const stave of staves) {
+    const beats = countQuarters(getTickables(stave, 'melodyNote'));
 
     var melodyVoice = new VF.Voice({ num_beats: beats, beat_value: 4 });
-    melodyVoice.addTickables(melodyNotes);
+    melodyVoice.addTickables(getTickables(stave, 'melodyNote'));
 
     var jianpuVoice = new VF.Voice({ num_beats: beats, beat_value: 4 });
-    jianpuVoice.addTickables(jianpuNotes);
+    jianpuVoice.addTickables(getTickables(stave, 'jianpuNote'));
 
     var lyricsVoice = new VF.Voice({ num_beats: beats, beat_value: 4 });
-    lyricsVoice.addTickables(lyricsNotes);
+    lyricsVoice.addTickables(getTickables(stave, 'lyricsNote'));
 
     voices.push([melodyVoice, jianpuVoice, lyricsVoice]);
   }
@@ -262,14 +288,13 @@ const staves = [makeStave(0)];
   // });
 
   const quarters = assignLyrics(testMelody, testLyrics)
-  const notes = prepareNotesForVoices(assignLengths(quarters));
-  const voices = makeVoices(notes);
+  const modelStaves = splitStaves(assignLengths(quarters));
+  const voices = makeVoices(modelStaves);
 
   // Now create all the needed staves. 
   for (let i = 1; i < voices.length; i++) {
     staves.push(makeStave(i));
   }
-
 
   for (let i = 0; i < voices.length; i++) {
     // Format and justify the notes to 400 pixels.
