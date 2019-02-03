4ce930692fe6f8151b4b4dcebce677f15bcfea34
Austin Chen <akrolsmir@gmail.com>
2019-02-02 19:08:20 -0800

    Consolidate the rhythmize logic for 4/4 and 8/4

--- rhythmize.js
+++ rhythmize.js
@@ -1,27 +1,9 @@
-// const markers4 = {
-//   ",": 1, 
-//   "L": "Sustain past 1",
-//   ".": 3,
-//   "[": "Sustain past 3",
-//   "_": "Extend to measure, then rest on 1"
-// }
-
-// const markers8 = {
-//   ",": 1,
-//   ",empty": 5,
-//   "L": "Sustain past 1",
-//   "Lempty": "Sustain past 5",
-//   ".": 7,
-//   "[]": "Sustain past 7",
-//   "_": "Sustain to measure, then rests"
-// }
-
 function rhythmize(input, timeSignature) {
   switch (timeSignature) {
     case TimeSignature.FOUR_FOUR:
-      return rhythmize4(input);
+      return rhythmizeBlock(input, timeSignature);
     case TimeSignature.EIGHT_FOUR:
-      return rhythmize8(input);
+      return rhythmizeBlock(input, timeSignature);
     case TimeSignature.FREE:
       return rhythmizeFree(input);
   }
@@ -40,91 +22,52 @@ function rhythmizeFree(input) {
   return output;
 }
 
-// Input: ["、", Note, Note, "。", Note, "_"...]
-// Output: ["|", Note, Note, "|"...], and also assign durations
-function rhythmize4(input) {
-  let output = [];
-  let block = [];
-  let lastBeat = 1;
-  for (let i = 0; i < input.length; i++) {
-    const symbol = input[i];
-    if (symbol == "、") {
-      [block, lastBeat] = processBlock(block, output, lastBeat, 1);
-      // Also add a new bar marker.
-      output.push(BAR);
-    }
-    else if (symbol == "。") {
-      [block, lastBeat] = processBlock(block, output, lastBeat, 3);
-    }
-    else if (symbol == "_") {
-      [block, lastBeat] = processBlock(block, output, lastBeat, 1);
-      // TODO: Instead, extend marked (prev) note into end of measure
-      // Also add a new bar marker and a rest.
-      output.push(BAR);
-      block.push(new RestNote()); 
-    }
-    else if (symbol == "▯") {
-      // Include the marked (next) note in the block
-      i++;
-      const markedNote = input[i];
-      block.push(markedNote);
-      // Then process as usual.
-      [block, lastBeat] = processBlock(block, output, lastBeat, 3);
-      // Next, add a quarter copy of the marked note, to sustain it through 3.
-      const copy = markedNote.getCopy();
-      copy.setDuration('4');
-      output.push(copy);
-      // The next beat is thus also adjusted by 1 quarter.
-      lastBeat++;
-    }
-    else if (symbol == "L") {
-      // Include the marked (next) note in the block
-      i++;
-      const markedNote = input[i];
-      block.push(markedNote);
-      // Then process as usual.
-      [block, lastBeat] = processBlock(block, output, lastBeat, 1);
-      // Add a bar marker.
-      output.push(BAR);
-      // Next, add a quarter copy of the marked note, to sustain it through 1.
-      const copy = markedNote.getCopy();
-      copy.setDuration('4');
-      output.push(copy);
-      // The next beat is thus also adjusted by 1 quarter.
-      lastBeat++;
-    }
-    else {
-      block.push(symbol);
-    }
+function getBeatForSymbol(symbol, timeSignature, lastBeat) {
+  const beats4 = {
+    "、": 1,
+    "。": 3,
+    "_": 1,  // Extend to measure, then rest on 1
+    "▯": 3, // Sustain past 3
+    "L": 1,  // Sustain past 1
   }
-  // Add any remaining notes to one last bar.
-  if (block.length > 0) {
-    [block, lastBeat] = processBlock(block, output, lastBeat, 1);
+  const beats8 = {
+    "、": 1,
+    "﹆": 5,
+    "。": lastBeat < 3 ? 3 : 7, // Can represent either 3 or 7
+    "_": 1, // Extend to measure, then rest on 1
+    "▯": lastBeat < 3 ? 3 : 7, // Sustain past midway
+    "L": 1, // Sustain past 1
+    "╚": 5, // Sustain past 5
   }
-  return output;
+  if (timeSignature == TimeSignature.FOUR_FOUR) {
+    return beats4[symbol];
+  } else {
+    return beats8[symbol];
+  }
 }
 
-function rhythmize8(input) {
+// Input: ["、", Note, Note, "。", Note, "_"...]
+// Output: ["|", Note, Note, "|"...], and also assign durations
+function rhythmizeBlock(input, timeSignature) {
+  const ts = timeSignature;
   let output = [];
   let block = [];
   let lastBeat = 1;
   for (let i = 0; i < input.length; i++) {
     const symbol = input[i];
     if (symbol == "、") {
-      [block, lastBeat] = processBlock(block, output, lastBeat, 1, 8);
+      [block, lastBeat] = processBlock(block, output, lastBeat, symbol, ts);
       // Also add a new bar marker.
       output.push(BAR);
     }
     else if (symbol == "﹆") {
-      [block, lastBeat] = processBlock(block, output, lastBeat, 5, 8);      
+      [block, lastBeat] = processBlock(block, output, lastBeat, symbol, ts);      
     }
     else if (symbol == "。") {
-      // This could represent either 3 or 7
-      const nextBeat = lastBeat < 3 ? 3 : 7;
-      [block, lastBeat] = processBlock(block, output, lastBeat, nextBeat, 8);
+      [block, lastBeat] = processBlock(block, output, lastBeat, symbol, ts);
     }
     else if (symbol == "_") {
-      [block, lastBeat] = processBlock(block, output, lastBeat, 1, 8);
+      [block, lastBeat] = processBlock(block, output, lastBeat, symbol, ts);
       // TODO: Instead, extend marked (prev) note into end of measure
       // Also add a new bar marker and a rest.
       output.push(BAR);
@@ -136,8 +79,7 @@ function rhythmize8(input) {
       const markedNote = input[i];
       block.push(markedNote);
       // Then process as usual.
-      const nextBeat = lastBeat < 3 ? 3 : 7;
-      [block, lastBeat] = processBlock(block, output, lastBeat, nextBeat, 8);
+      [block, lastBeat] = processBlock(block, output, lastBeat, symbol, ts);
       // Next, add a quarter copy of the marked note, to sustain it.
       const copy = markedNote.getCopy();
       copy.setDuration('4');
@@ -151,7 +93,7 @@ function rhythmize8(input) {
       const markedNote = input[i];
       block.push(markedNote);
       // Then process as usual.
-      [block, lastBeat] = processBlock(block, output, lastBeat, 1, 8);
+      [block, lastBeat] = processBlock(block, output, lastBeat, symbol, ts);
       // Add a bar marker.
       output.push(BAR);
       // Next, add a quarter copy of the marked note, to sustain it through 1.
@@ -167,7 +109,7 @@ function rhythmize8(input) {
       const markedNote = input[i];
       block.push(markedNote);
       // Then process as usual.
-      [block, lastBeat] = processBlock(block, output, lastBeat, 5, 8);
+      [block, lastBeat] = processBlock(block, output, lastBeat, symbol, ts);
       // Next, add a quarter copy of the marked note, to sustain it.
       const copy = markedNote.getCopy();
       copy.setDuration('4');
@@ -181,12 +123,14 @@ function rhythmize8(input) {
   }
   // Add any remaining notes to one last bar.
   if (block.length > 0) {
-    [block, lastBeat] = processBlock(block, output, lastBeat, 1, 8);
+    [block, lastBeat] = processBlock(block, output, lastBeat, "、", ts);
   }
   return output;
 }
 
-function processBlock(block, output, lastBeat, thisBeat, quarters = 4) {
+function processBlock(block, output, lastBeat, symbol, timeSignature) {
+  const thisBeat = getBeatForSymbol(symbol, timeSignature, lastBeat);
+  const quarters = timeSignature == TimeSignature.FOUR_FOUR ? 4 : 8;
   assignDurations(block, between(lastBeat, thisBeat, quarters));
   output.push(...block);
   return [[], thisBeat];
