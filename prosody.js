92580cb9bfd87f971408368d249221d60d76d878
Austin Chen <akrolsmir@gmail.com>
2019-04-18 19:18:00 -0700

    Show the melody differences between notes

--- prosody.js
+++ prosody.js
@@ -17,6 +17,22 @@ const gongcheToJianpu = {
   "仜": ".3",
 }
 
+const jianpuToOffset = {
+  "5.": -3,
+  "6.": -2,
+  "7.": -1,
+  "1": 0,
+  "2": 1,
+  "3": 2,
+  "4": 3,
+  "5": 4,
+  "6": 5,
+  "7": 6,
+  ".1": 7,
+  ".2": 8,
+  ".3": 9,
+}
+
 const beatSymbols = [ "、", "。"];
 
 const rowHeaders = [
@@ -25,21 +41,32 @@ const rowHeaders = [
   {id: 'tone', display: '聲調'},
   {id: 'yinyang', display: '陰陽'},
   {id: 'beats', display: '板眼'},
-  {id: 'melody', display: '簡譜音高'}
+  {id: 'melody', display: '簡譜音高'},
+  // How much a note is offset relative to previous note.
+  {id: 'difference', display: 'difference'} 
 ]
 
-function parseMelodyChunk(melodyChunk) {
+function parseMelodyChunk(melodyChunk, lastOffset) {
   const melody = [];
   const beats = [];
+  const differences = [];
   for (const char of melodyChunk) {
     if (beatSymbols.includes(char)) {
       beats.push(char);
     }
     else if (char in gongcheToJianpu) {
       melody.push(gongcheToJianpu[char]);
+      const offset = jianpuToOffset[gongcheToJianpu[char]];
+      // Explicit check against undefined, since 0 is a valid offset value.
+      differences.push(lastOffset == undefined ? 0 : offset - lastOffset);
+      lastOffset = offset;
     }
   }
-  return { beats: beats.join(' '), melody: melody.join(' ') };
+  return [{ 
+    beats: beats.join(' '), 
+    melody: melody.join(' '),
+    difference: differences.map(r => (r <= 0 ? '' : '+') + r).join(' ') 
+  }, lastOffset];
 }
 
 /** @returns the lines of these songs, zipped (all line 1s, then 2s, 3s...) */
@@ -69,6 +96,8 @@ function buildLines(song) {
   const lines = [];
   let lineCount = 1;
   let line = { words: [], song, index: lineCount };
+  let lastOffset;
+  let melodyObject;
   for (const lyric of song.lyrics) {
     if (lyric != '\n') {
       const rhyme = RHYME_MAP[lyric] ? RHYME_MAP[lyric] : [,,,,,];
@@ -76,7 +105,8 @@ function buildLines(song) {
       const word = { lyric, yinyang, tone };
       word.pronounce = song.region == 'North' ? north : south;
       // Parse the beats and jianpu, and copy into word object.
-      Object.assign(word, parseMelodyChunk(melodyChunks[melodyIndex]));
+      [melodyObject, lastOffset] = parseMelodyChunk(melodyChunks[melodyIndex], lastOffset);
+      Object.assign(word, melodyObject);
       melodyIndex++;
       line.words.push(word);
     }
