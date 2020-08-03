3f85d2dfd4e0d45fd392785c6e10be0240dde88e
Austin Chen <akrolsmir@gmail.com>
2020-08-02 19:54:57 -0700

    Prettier song.js

--- models/song.js
+++ models/song.js
@@ -1,7 +1,17 @@
-import { RHYME_MAP } from "../assets/rhyme_dictionary.js";
+import { RHYME_MAP } from '../assets/rhyme_dictionary.js';
 
 export class Song {
-  constructor(id, title, composer, pageNum, lyrics, melody, region, fullLyrics, modeKey) {
+  constructor(
+    id,
+    title,
+    composer,
+    pageNum,
+    lyrics,
+    melody,
+    region,
+    fullLyrics,
+    modeKey
+  ) {
     this.id = id;
     this.title = title;
     this.composer = composer;
@@ -13,7 +23,7 @@ export class Song {
     // TODO: Since lyrics are derivable from fullLyrics,
     // stop storing lyrics in the cloud; just compute it.
     this.fullLyrics = fullLyrics ? fullLyrics : this.lyrics;
-    this.modeKey = modeKey ? modeKey : 'default_mode_key'
+    this.modeKey = modeKey ? modeKey : 'default_mode_key';
   }
   toJson() {
     return {
@@ -25,17 +35,27 @@ export class Song {
       melody: this.melody,
       region: this.region,
       fullLyrics: this.fullLyrics,
-      modeKey: this.modeKey
+      modeKey: this.modeKey,
     };
   }
   static fromJson(json) {
-    return new Song(json.id, json.title, json.composer, json.pageNum, 
-      json.lyrics, json.melody, json.region, json.fullLyrics, json.modeKey);
+    return new Song(
+      json.id,
+      json.title,
+      json.composer,
+      json.pageNum,
+      json.lyrics,
+      json.melody,
+      json.region,
+      json.fullLyrics,
+      json.modeKey
+    );
   }
   // Return the locations of all rhyming lyrics (lyrics followed by '.');
   getRhymeIndices() {
-    return this.fullLyrics.split('')
-      .flatMap((char, i) => (char == '.' && i > 0) ? i - 1 : []);
+    return this.fullLyrics
+      .split('')
+      .flatMap((char, i) => (char == '.' && i > 0 ? i - 1 : []));
   }
   hasRhymeCategory(query) {
     for (const category of this.getRhymeCategories(RHYME_MAP)) {
@@ -47,9 +67,9 @@ export class Song {
   }
   getRhymeCategories() {
     const categories = this.getRhymeIndices()
-      .map(index => this.fullLyrics[index])
-      .filter(lyric => RHYME_MAP[lyric])
-      .map(lyric => RHYME_MAP[lyric][0]);
+      .map((index) => this.fullLyrics[index])
+      .filter((lyric) => RHYME_MAP[lyric])
+      .map((lyric) => RHYME_MAP[lyric][0]);
     // Deduplicate with an intermediate set.
     return [...new Set(categories)];
   }
